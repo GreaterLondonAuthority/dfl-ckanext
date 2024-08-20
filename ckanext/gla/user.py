3cfe9284aab7307f7f77d4b2608ee7e355c0ad0b
Yinuo-M <mengyn1999@gmail.com>
2024-08-20 16:32:52 +0100

    feat: (DAT-801) create user_create chained action to make email and name lowercase a new user registers

---
+++ ckanext/gla/user.py
@@ -0,0 +1,7 @@
+import ckan.plugins.toolkit as toolkit
+
+@toolkit.chained_action
+def user_create(original_action, context, data_dict):
+    data_dict['email'] = data_dict.get("email").lower()
+    data_dict['name'] = data_dict.get('name').lower()
+    return original_action(context, data_dict)
