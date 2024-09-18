4050e738919c3d97b26bfeba6232dd07e9f6c707
Rick Moynihan <rick.m@swirrl.com>
2024-09-18 16:14:43 +0100

    WIP display orgs in table (needs munged)

--- ckanext/gla/user.py
+++ ckanext/gla/user.py
@@ -1,6 +1,7 @@
 import ckan.lib.helpers as h
 import ckan.model as model
 from ckan.model.user import User
+from ckan.model.group import Member, Group
 
 import ckan.plugins.toolkit as toolkit
 from ckan.common import logout_user, request
@@ -50,4 +51,32 @@ def user_list(original_action, context, data_dict):
     # Modify the query to add the 'plugin_extras' field from the User model
     query = query.add_columns(User.plugin_extras)
 
+    # Join with the Member table to find user memberships
+    # query = query.join(Member, sa.and_(
+    #     Member.table_id == sa.cast(User.id, sa.String),
+    #     Member.table_name == 'user'
+    # ))
+
+    # Left join with the Member table to find user memberships, if any
+    query = query.outerjoin(Member, sa.and_(
+        Member.table_id == sa.cast(User.id, sa.String),
+        Member.table_name == 'user'
+    ))    
+
+    
+    # Join with the Group (organization) table
+    query = query.outerjoin(Group, Group.id == Member.group_id)
+
+
+    # Modify the query to return the user's plugin_extras and organization names
+    query = query.add_columns(User.plugin_extras, Group.name.label('organization_name'))
+
+    
+    
+    # Modify the query to add 'plugin_extras' and the boolean for organization membership
+    # query = query.add_columns(User.plugin_extras, is_org_member)    
+    
+    # Return the modified query
+    print(type(user_list))
+    print(user_list)
     return query
