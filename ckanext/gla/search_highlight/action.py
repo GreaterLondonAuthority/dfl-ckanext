GreaterLondonAuthority/dfl-ckanext

commit 3f71a4045d0c720788fec24fc67da99a34d6eaf2

Fix DAT-845 bug with facets on /organization/bulk_process/:id route
Prior to fix selecting facet values on this page raised a 500 exception

ckanext/gla/search_highlight/action.py
@@ -56,8 +56,9 @@ def filtered_facets(all_facets):
 
     for (facet, vals) in selected_facets().items():
         for v in vals:
-            if v not in non_zero_or_selected_facets[facet]:
-                non_zero_or_selected_facets[facet][v] = 0
+            if v not in non_zero_or_selected_facets.get('facet',[]):
+                if non_zero_or_selected_facets.get('facet'):
+                    non_zero_or_selected_facets['facet'][v] = 0
     
     return non_zero_or_selected_facets
 

commit a1b7e379f02dcdfc43a271a518f31f99d778419e

Fix DAT-750 regression
Bisecting showed the regression was introduced by commit 3f71a404

ckanext/gla/search_highlight/action.py
@@ -57,14 +57,15 @@ def selected_facets():
 # Filter facets so values are only provided for those that have
 # either counts > 0 or are selected on the users requested.
 def filtered_facets(all_facets):
+    # first filter out all zero count facets
     non_zero_or_selected_facets = {k: {ik: iv for ik, iv in v.items() if iv > 0} for k, v in all_facets.items() if isinstance(v, dict)}
-
-    for (facet, vals) in selected_facets().items():
+    # insert into non_zero facets any selected facets (including ones with count 0)
+    for (selected_facet, vals) in selected_facets().items():
         for v in vals:
-            if v not in non_zero_or_selected_facets.get('facet',[]):
-                if non_zero_or_selected_facets.get('facet'):
-                    non_zero_or_selected_facets['facet'][v] = 0
-    
+            if v not in non_zero_or_selected_facets.get(selected_facet,[]):
+                non_zero_or_selected_facets[selected_facet] = {v:0}
+                #non_zero_or_selected_facets[facet][v] = 0
+                    
     return non_zero_or_selected_facets
 
 